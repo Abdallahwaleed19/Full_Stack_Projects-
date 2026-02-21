37c0be108ea1cfb67ff43230db18a4208a1c5b02
Abdallah Waleed Kamal <waleedabdallah238@gmail.com>
2026-02-21 07:11:03 +0200

    Delete library_Managment directory

--- library_Managment/author.py
+++
@@ -1,8 +0,0 @@
-class Author:
-    def __init__(self, id: int, name: str, biography: str):
-        self.id = id
-        self.name = name
-        self.biography = biography
- 
-    def write_book(self) -> None:
-        print(f"Author {self.name} is writing a new book") 
--- library_Managment/book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, id_book: int, title: str, author_id: int):
-        self.id_book = id_book
-        self.title = title
-        self.availability = True
-        self.author_id = author_id
-    
-    def is_available(self) -> bool:
-        return self.availability 
