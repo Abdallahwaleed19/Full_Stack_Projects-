5e90c6dff06300430282e4eb5098f1df99ad215b
Abdallah Waleed Kamal <waleedabdallah238@gmail.com>
2026-02-21 07:11:27 +0200

    Delete Django directory

--- Django/Lec1/My_Books/Books/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('books',views.Books,name='books'),
-    path('author',views.Author,name='author'),
-]
--- Django/Lec1/My_Books/Books/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home (request):
-    return HttpResponse("Home Page")
-def Books (request):
-    return HttpResponse("Books Page")
-def Author (request):
-    return HttpResponse("Author Page")
